043e3bf764cde3e455eaa6b1a307e14fd72ce35a
muhrifat <muh.rifat2@gmail.com>
2016-03-25 10:19:40 +0700

    cek kartubarangcontroller,

--- app/Http/Controllers/EkstraController.php
+++ app/Http/Controllers/EkstraController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Item;
+use App\Transaction;
 use Request;
 use Illuminate\Support\Facades\Input;
 use Excel;
@@ -22,27 +23,67 @@ class EkstraController extends Controller
     		$excel->setTitle('DATA SELURUH ITEM');
     		$excel->sheet('DATA ITEM', function($sheet) use($items){
     			$row = 7;
-    			//$sheet->fromArray($items);
-    			$sheet->row($row, array(
-    				'NO',
-    				'NOMOR BARANG',
-    				'NAMA',
-    				'SPEK',
-    				'SALDO',
-    				'UM'
-    			));
-    			$no = 1;
-    			foreach ($items as $data){
-    				$sheet->row(++$row, array(
-    					$no++,
-    					$data->no_part,
-    					$data->name,
-    					$data->spec,
-    					$data->stock,
-    					$data->pieces
-    				));
-    			}
-    		});
-    	})->export('xls');
+                $sheet->setWidth(array(
+                    'B' => 4, 
+                    'C' => 20,
+                    'D' => 20,
+                    'E' => 20,
+                    'F' => 11,
+                    'G' => 10
+                    ));
+
+                $sheet->cells('B7:G10', function($cells) {
+                    // $cells->setBorder('thin','thin','thin','thin');
+
+                    $cells->setBorder(array(
+                        'borders' => array(
+                            'top'   => array(
+                                'style' => 'thin'
+                                ),
+                            )
+                        ));
+                    $cells->setAlignment('center');
+                });
+
+                $sheet->row($row, array(
+                    null,
+                    'NO',
+                    'NOMOR BARANG',
+                    'NAMA',
+                    'SPEK',
+                    'SALDO',
+                    'UM'
+                    ));
+
+
+                $no = 1;
+                foreach ($items as $data){
+                    $sheet->row(++$row, array(
+                        null,
+                        $no++,
+                        $data->no_part,
+                        $data->name,
+                        $data->spec,
+                        $data->stock,
+                        $data->pieces
+                        ));
+                }
+            });
+})->export('xls');
+}
+
+public function printbon($tgl){
+        //part no, part description, type/spec, qty, um, notes
+        /*
+
+        select i.no_part, i.name, i.spec, t.tmp_stock, s.desc
+        from transaction t, item i, status s
+        where time = $time and t.itemid = i.itemid s.statusid = t.statusid;
+        */
+        // $items = Transaction::select('
+        //     select i.no_part, i.name, i.spec, t.tmp_stock, s.desc
+        //     from transaction t, item i, status s
+        //     where time = $time and t.itemid = i.itemid s.statusid = t.statusid
+        //     ')->;
     }
 }
