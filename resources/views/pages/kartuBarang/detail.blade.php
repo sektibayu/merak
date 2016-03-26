3b5f46ee582d397b88cc275c412fe02f9ad5fb9d
sektibayu <sektibayu@gmail.com>
2016-03-26 10:41:55 +0700

    perbaikan kecil

--- resources/views/pages/kartuBarang/detail.blade.php
+++ resources/views/pages/kartuBarang/detail.blade.php
@@ -90,7 +90,7 @@
                                 </div>
                                 <div class="modal-body">
                                    <!-- isi table -->
-                                   <form action="" method="" class="form-horizontal">
+                                   <form action="" method="post" class="form-horizontal">
                                         <div class="box-body">
                                             <div class="form-group">
                                                 <div class="col-sm-7">
